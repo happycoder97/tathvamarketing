0d4a800a7dc508f8994f4e8eba9975db9387d9c3
Salih <salihpdm.0036@gmail.com>
2019-06-02 22:28:04 +0530

    Carousal Improved

--- js/bs-animation.js
+++ js/bs-animation.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
+  AOS.init({ disable: 'mobile' });
+
+  // Avoiding glitch in carousel slide transition due to undefined height
+  const height = $('.carousel-inner').css("height");
+  const width = $('.carousel-inner').css("width");
+  $('.carousel .img-div').css("width", width);
+  $('.carousel .img-div').css("height", height);
 });
